20c51a1454721d44e46a8b783fbadc42080a8c15
Boyiiiisun <boyisun592@gmail.com>
2026-02-07 18:39:52 -0500

    Update table and DB_create file

--- Database/DB_create.sql
+++ Database/DB_create.sql
@@ -22,8 +22,8 @@ CREATE TABLE users (
     description TEXT, 
     message_id INT,  
   
-    --anxiety_level INT DEFAULT 5,  -- Optional: 1-10 scale for anxiety level
-    --likes INT DEFAULT 0,  -- Optional: Count of likes for the message
+    -- anxiety_level INT DEFAULT 5,  -- Optional: 1-10 scale for anxiety level
+    -- likes INT DEFAULT 0,  -- Optional: Count of likes for the message
     
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
